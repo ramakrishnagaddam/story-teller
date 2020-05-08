f4ef9ab15e8adab4e9c67581959c09fca2219be4
Rama Krishna Gaddam <ramakrishnagaddam93@gmail.com>
2020-05-08 18:31:29 +0530

    Story.model to store the stores under a volume

---
+++ model/Story.model.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('Story', Schema({
+    _id: Schema.Types.ObjectId,
+    storyName: String,
+    storyDesc: String,
+    storyImage: String,
+    credits: String,
+    storyURL: String,
+    volume: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: 'Volume'
+    }
+}));
